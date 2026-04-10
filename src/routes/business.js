e7a32ea6443528f728ebe5216a205c6939643cfd
Steveng1997 <stivengarcia1855@gmail.com>
2026-04-10 18:05:38 -0500

    chsange 120

--- src/routes/business.js
+++ src/routes/business.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const biz = require("../controllers/businessController");
 
 router.post("/confirm-payment", biz.confirmPayment);
+router.get("/goals", biz.getGoals);
+router.post("/goals", biz.upsertGoal);
 
 module.exports = router;
