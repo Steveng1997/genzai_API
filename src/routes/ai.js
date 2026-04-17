43b3a08a097d2143c13239803ad19c44db379cc0
Steveng1997 <stivengarcia1855@gmail.com>
2026-04-17 16:05:58 -0500

    change 161

--- src/routes/ai.js
+++ src/routes/ai.js
@@ -6,7 +6,7 @@ const aiController = require("../controllers/aiController");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, "/tmp");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
