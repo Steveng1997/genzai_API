ddf5c91434dbb14a6427083f3421ab77ff2b9509
Steveng1997 <stivengarcia1855@gmail.com>
2026-04-22 12:20:09 -0500

    change 219

--- src/routes/services.js
+++ src/routes/services.js
@@ -4,8 +4,8 @@ const serviceController = require("../controllers/servicesController");
 
 router.post("/save", serviceController.createService);
 router.get("/tenant/:tenantId", serviceController.getServicesByTenant);
-router.get("/:tenantId/:serviceId", serviceController.getServiceById);
 router.get("/count/:tenantId", serviceController.countServicesByTenant);
+router.get("/:tenantId/:serviceId", serviceController.getServiceById);
 router.put("/update", serviceController.updateService);
 router.delete("/delete/:tenantId/:serviceId", serviceController.deleteService);
 
